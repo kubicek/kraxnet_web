48864aeb1dcb2d9c1957465eec4c923bc9766b65
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2008-10-20 00:50:19 +0200

    capistrano deployment

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+set :application, "kraxnet"
+set :repository, "git://github.com/kubicek/kraxnet_web.git"
+set :scm, "git"
+
+role :web, "server3.railshosting.cz"
+role :app, "server3.railshosting.cz"
+role :db,  "server3.railshosting.cz", :primary => true
+
+set :deploy_to, "/home/kraxnet/app/"
+set :user, "kraxnet"
+
+set :use_sudo, false
+
+namespace :deploy do
+  task :start, :roles => :app do
+  end
+end
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
