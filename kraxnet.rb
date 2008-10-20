9a29ce95363b3169e1e10b8228351562c5f74e12
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2008-10-20 14:43:52 +0200

    Novinky z blogu

--- kraxnet.rb
+++ kraxnet.rb
@@ -1,10 +1,23 @@
 require 'rubygems'
 require 'sinatra'
+require 'open-uri'
+require 'rss'
  
 get '/' do
   redirect "/sluzby"
 end
 
+get '/novinky' do
+  feed_url = 'http://blog.xnet.cz/feed/'
+  open(feed_url) do |http|
+    response = http.read
+    result = RSS::Parser.parse(response, false)
+    @blog_items = result.items[0..10]
+  end
+  erb :rss_reader
+end
+
 get '/:id' do
   erb params[:id].gsub(/-/,'').to_sym
 end
+
