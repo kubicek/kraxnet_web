088d59238b11d1f984518495124dc22bfd09b04a
Karel Minarik <karmi@karmi.cz>
2009-02-12 23:24:50 +0100

    Refactored blog loading logic, so that route block is clean

--- kraxnet.rb
+++ kraxnet.rb
@@ -3,17 +3,21 @@ require 'sinatra'
 require 'open-uri'
 require 'rss'
 
+module Kraxnet
+  class BlogFeed
+    FEED_URL = 'http://blog.xnet.cz/feed/'
+    def self.load
+      open(FEED_URL) { |http| RSS::Parser.parse(http.read, false).items[0..10] }
+    end
+  end
+end
+
 get '/' do
   erb :index
 end
 
 get '/novinky' do
-  feed_url = 'http://blog.xnet.cz/feed/'
-  open(feed_url) do |http|
-    response = http.read
-    result = RSS::Parser.parse(response, false)
-    @blog_items = result.items[0..10]
-  end
+  @blog_items = Kraxnet::BlogFeed.load
   @page_title = "Novinky"
   erb :rss_reader
 end
